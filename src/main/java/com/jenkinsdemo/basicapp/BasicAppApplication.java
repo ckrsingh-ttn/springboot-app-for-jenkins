945003c0c5cb76ddb24e1777088543f62856ce2c
Chandan <chandan.kumar@tothenew.com>
2022-01-27 00:09:38 +0530

    version 2

--- src/main/java/com/jenkinsdemo/basicapp/BasicAppApplication.java
+++ src/main/java/com/jenkinsdemo/basicapp/BasicAppApplication.java
@@ -11,7 +11,7 @@ public class BasicAppApplication {
 
 	@GetMapping("/")
 	String index(){
-		return "Hello Jenkins V1";
+		return "Hello Jenkins V2";
 	}
 
 	public static void main(String[] args) {
